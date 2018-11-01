bcf80e98c314a7305189b5f0c80996c53188ea4f
Michael Huang <myh1000@gmail.com>
2018-10-31 23:53:54 -0700

    wrote the encoder for vae implementation

--- michael/utils.py
+++ michael/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pypianoroll import Multitrack, Track
+import pypianoroll
 from matplotlib import pyplot as plt
 import pygame
 import os
@@ -9,16 +10,26 @@ pygame.init()
 
 # loaded.tracks[1].pianoroll.size # check size to see if instrument is used
 #.binarize() makes pitch irrelevant, might be easier to do at first w/o worring about pitch
-# SHAPE (time,pitch) (-1,128), 24 steps per beat
+# SHAPE (time,pitch) (-1,128), 24 steps/times per beat
 # for track in tracks:
     # if tracks.pianoroll.size < 100
 
-def play(midi):
+songs = np.load('songs.npy')
+
+def load(npz):
+    return pypianoroll.load(npz)
+
+def play(midi='temp.mid'): # midi name
     pygame.mixer.music.load(midi)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         playing = True
     playing = False
+
+write = Multitrack.write
+def write_2(self, name='temp.mid'):
+    write(self,name)
+Multitrack.write = write_2
 
 def stop():
     pygame.mixer.music.stop()
