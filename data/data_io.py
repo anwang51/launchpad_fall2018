2e4519e9fdb08dc42d55b80e275b878f5f125760
Praveen Batra <praveenbatra@icloud.com>
2018-11-16 23:50:08 -0800

    Fix bugs

--- data/data_io.py
+++ data/data_io.py
@@ -15,26 +15,27 @@ def iter_dir(dir, ext=None, ext_set=None, recursive=True):
 
 lpd5_valid_tracks = ['Drums', 'Piano', 'Guitar', 'Bass', 'Strings']
 
-def iter_lpd5_dataset(root_dir, track_name='Piano', min_len=None):
+def iter_lpd5_dataset(root_dir, track_name='Piano', split_len=None):
     # track_name should be one of lpd5_valid_tracks
     for path in iter_dir(root_dir, '.npz'):
         multitrack = pypianoroll.load(path)
         if multitrack:
             tracks = (track for track in multitrack.tracks if track.name == track_name)
             for track in tracks:
-                if min_len:
-                    yield from midi_proc.split_silence(track.pianoroll, min_len=min_len)
+                if split_len:
+                    yield from midi_proc.split_silence(track.pianoroll, split_len=split_len)
                 else:
-                    yield track
+                    yield track.pianoroll
 
-def iter_midi_dataset(root_folder, allowed_programs=range(0, 5), min_len=None):
+def iter_midi_dataset(root_folder, allowed_programs=range(0, 5), split_len=None):
     for path in iter_dir(root_folder, '.mid'):
         midi = midi_proc.load_midi(path)
         if midi:
             for track in midi.tracks:
                 if midi_proc.program(track) in allowed_programs:
                     vectorized = midi_proc.vectorize_track(track, midi.ticks_per_beat)
-                    if min_len:
-                        yield from midi_proc.split_silence(vectorized, min_len)
-                    else:
-                        yield track
+                    if vectorized is not None:
+                        if split_len:
+                            yield from midi_proc.split_silence(vectorized, split_len)
+                        else:
+                            yield vectorized
--- data/midi_proc.py
+++ data/midi_proc.py
@@ -12,6 +12,8 @@ import mido
 import pypianoroll
 import time
 
+out = None
+
 def load_npz(path):
     try:
         return pypianoroll.load(path)
@@ -29,7 +31,7 @@ def num_frames(ticks, ticks_per_beat, note_denominator):
     return int((note_denominator/4 * ticks) // ticks_per_beat)
 
 def is_note(event):
-    return event.type == 'note_on' and event.type == 'note_off'
+    return event.type == 'note_on' or event.type == 'note_off'
 
 def is_drum(note):
     return is_note(note) and note.channel == 9
@@ -85,7 +87,7 @@ def trim_silence(frames):
     if first_nonzero_index is not None and last_nonzero_index is not None:
         return frames[first_nonzero_index:last_nonzero_index+1]
 
-def split_silence(frames, min_len=16):
+def split_silence(frames, split_len=16):
     """
     >>> arr = np.array([[1], [0], [0], [2], [0], [3], [4], [5], [0], [0], [0], [6], [0]])
     >>> [list(x) for x in split_by_silence(arr, min_len=2)]
@@ -97,7 +99,7 @@ def split_silence(frames, min_len=16):
     split_frames = list()
     for index, frame in enumerate(frames):
         if np.any(frame):
-            if num_silence >= min_len:
+            if num_silence >= split_len:
                 if left is not None and right is not None:
                     split_frames.append(frames[left:right])
                 left = index
@@ -105,16 +107,19 @@ def split_silence(frames, min_len=16):
             right = index + 1
         else:
             num_silence += 1
-    if num_silence >= min_len:
+    if num_silence >= split_len:
         split_frames.append(frames[left:right])
     else:
         split_frames.append(frames[left:])
     return [frames for frames in split_frames if not len(frames) == 0]
     
-def play_vector(vector):
+def play_vector(vector, delay=0.1):
+    global out
+    if out is None:
+        out = mido.open_output("play_vector", virtual=True)
     for row in vector:
         for note, vel in enumerate(row):
-            out.send(mido.Message('note_on'), note=note, velocity=int(128*vel))
-        time.sleep(0.1)
+            out.send(mido.Message('note_on', note=note, velocity=int(128*vel)))
+        time.sleep(delay)
         for note in range(len(row)):
-            out.send(mido.Message('note_off'), note=note)
+            out.send(mido.Message('note_off', note=note))
